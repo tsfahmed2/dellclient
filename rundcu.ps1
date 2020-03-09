c73eff28cb79a8bf0f2bb9ab23c7491a18e53f67
tsfahmed2 <tsfahmed2@gmail.com>
2020-03-09 11:10:19 -0500

    Update rundcu.ps1

--- rundcu.ps1
+++ rundcu.ps1
@@ -1,4 +1,3 @@
-
 <#	
 	.NOTES
 	===========================================================================
@@ -9,41 +8,43 @@
 	 Filename:     	
 	===========================================================================
 	.DESCRIPTION
-		Dell command update - install and run it.
+		Dell command update install and run it.
 #>
+
 
 
 if (![System.Environment]::Is64BitProcess)
 {
-     # start new PowerShell as x64 bit process, wait for it and gather exit code and standard error output
-    $sysNativePowerShell = "$($PSHOME.ToLower().Replace("syswow64", "sysnative"))\powershell.exe"
-
-    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = $sysNativePowerShell
-    $pinfo.Arguments = "-ex bypass -file `"$PSCommandPath`""
-    $pinfo.RedirectStandardError = $true
-    $pinfo.RedirectStandardOutput = $true
-    $pinfo.CreateNoWindow = $true
-    $pinfo.UseShellExecute = $false
-    $p = New-Object System.Diagnostics.Process
-    $p.StartInfo = $pinfo
-    $p.Start() | Out-Null
-
-    $exitCode = $p.ExitCode
-
-    $stderr = $p.StandardError.ReadToEnd()
-
-    if ($stderr) { Write-Error -Message $stderr }
+	# start new PowerShell as x64 bit process, wait for it and gather exit code and standard error output
+	$sysNativePowerShell = "$($PSHOME.ToLower().Replace("syswow64", "sysnative"))\powershell.exe"
+	
+	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+	$pinfo.FileName = $sysNativePowerShell
+	$pinfo.Arguments = "-ex bypass -file `"$PSCommandPath`""
+	$pinfo.RedirectStandardError = $true
+	$pinfo.RedirectStandardOutput = $true
+	$pinfo.CreateNoWindow = $true
+	$pinfo.UseShellExecute = $false
+	$p = New-Object System.Diagnostics.Process
+	$p.StartInfo = $pinfo
+	$p.Start() | Out-Null
+	
+	$exitCode = $p.ExitCode
+	
+	$stderr = $p.StandardError.ReadToEnd()
+	
+	if ($stderr) { Write-Error -Message $stderr }
 }
 else
 {
-    $transcriptlog = "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1",".log"))"
-
-    # start logging to TEMP in file "scriptname".log
-    Start-Transcript -Path $transcriptlog | Out-Null
-
-
-    function Get-Architecture {
+	$transcriptlog = "$env:TEMP\$($(Split-Path $PSCommandPath -Leaf).ToLower().Replace(".ps1", ".log"))"
+	
+	# start logging to TEMP in file "scriptname".log
+	Start-Transcript -Path $transcriptlog | Out-Null
+	
+	
+	function Get-Architecture
+	{
         <#
             .SYNOPSIS
                 Get-Architecture
@@ -57,16 +58,18 @@ else
             .NOTES
                 Additional information about the function.
         #>
-            
-            [CmdletBinding()][OutputType([string])]
-            param ()
-            
-            $OSArchitecture = (Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture).OSArchitecture
-            Return $OSArchitecture
-            #Returns 32-bit or 64-bit
-        }
-        
-        function Get-DellCommandUpdateLocation {
+		
+		[CmdletBinding()]
+		[OutputType([string])]
+		param ()
+		
+		$OSArchitecture = (Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture).OSArchitecture
+		Return $OSArchitecture
+		#Returns 32-bit or 64-bit
+	}
+	
+	function Get-DellCommandUpdateLocation
+	{
         <#
             .SYNOPSIS
                 Find dcu-cli.exe
@@ -75,64 +78,70 @@ else
                 Locate dcu-cli.exe as it may reside in %PROGRAMFILES% or %PROGRAMFILES(X86)%
             
         #>
-            
-            [CmdletBinding()][OutputType([string])]
-            param ()
-            
-            $Architecture = Get-Architecture
-            If ($Architecture -eq "32-bit") {
-                $File = Get-ChildItem -Path $env:ProgramFiles -Filter "dcu-cli.exe" -ErrorAction SilentlyContinue -Recurse
-            } else {
-                $File = Get-ChildItem -Path ${env:ProgramFiles(x86)} -Filter "dcu-cli.exe" -ErrorAction SilentlyContinue -Recurse
-            }
-            Return $File.FullName
-        }
-            
-        #Find dcu-cli.exe
-        $EXE = Get-DellCommandUpdateLocation
-        $dclogfolder = $env:SystemDrive + '\' + 'DCUpdate'
-        New-Item -ItemType Directory $dclogfolder -force
-        $outlog =  $dclogfolder + '\' + 'dellcuoutput.log'
-        $scanlog = $dclogfolder + '\' + 'scanlog.log'
-        $setlog = $dclogfolder + '\' + 'setdcu.log'
-        if ((Get-CimInstance win32_bios).Manufacturer -match 'Dell')  {
-            if (($EXE -eq $null) -or ((Get-Item $EXE).VersionInfo.FileVersion -ne '3.1.0.64')) {
-                    ###Code to download andinstallstuff starts here
-                New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
-                New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 2
-                
-                Write-Host "Dell command update not installed"
-                Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER05944435M/1/Dell-Command-Update_0NJ7C_WIN_3.1.0_A00.EXE" -OutFile "$env:temp\DCU.exe" -UseBasicParsing -Verbose
-                start-process -FilePath "$env:temp\DCU.exe" -ArgumentList '/s' -Wait
-                Start-Sleep -Seconds 5
-            }
-            #elseif ((Get-Item $EXE).VersionInfo.FileVersionon -eq '3.1.0.64') {
-            #$dcuversion = (Get-Item $EXE).VersionInfo.FileVersion
-            $EXE = Get-DellCommandUpdateLocation
-            Write-Host "*******************Dcu cli located at $EXE*****************"
-            Write-Host "******************Set dell command update settings*********************"
-            $a = "/configure -updateSeverity=critical,recommended,optional -scheduleWeekly=Mon,11:45 -updatetype=driver,bios,firmware,others -updateDeviceCategory=network,storage,audio,video,input,chipset -scheduleAction=DownloadInstallAndNotify -autoSuspendBitLocker=enable -outputLog=$setlog"
-            $set = Start-Process -FilePath $EXE -ArgumentList $a -WindowStyle Hidden -Wait -Passthru #| Out-Null
-            $set.WaitForExit()
-    
-            Write-Host "******************Scanning for updates*********************"
-            $b = "/scan -outputLog=$scanlog"
-            $scan = Start-Process -FilePath $EXE -ArgumentList $b -WindowStyle Hidden -Wait -PassThru 
-            $scan.WaitForExit()
-          
-            Write-Host "******************Applying updates*************************"
-            $c = "/applyUpdates -silent -outputLog=$outlog"
-            $apply = Start-Process -FilePath $EXE -ArgumentList $c -WindowStyle Hidden -Wait -PassThru
-            $apply.WaitForExit()
-            Copy-Item $dclogfolder\*.log -Destination $env:TEMP -Force -Recurse
-            Remove-Item $dclogfolder -Force -Recurse
-            
-        }
-        else {
-            Write-Host "This is not a dell workstation"
-        }
-    Stop-Transcript | Out-Null
+		
+		[CmdletBinding()]
+		[OutputType([string])]
+		param ()
+		
+		$Architecture = Get-Architecture
+		If ($Architecture -eq "32-bit")
+		{
+			$File = Get-ChildItem -Path $env:ProgramFiles -Filter "dcu-cli.exe" -ErrorAction SilentlyContinue -Recurse
+		}
+		else
+		{
+			$File = Get-ChildItem -Path ${env:ProgramFiles(x86)} -Filter "dcu-cli.exe" -ErrorAction SilentlyContinue -Recurse
+		}
+		Return $File.FullName
+	}
+	
+	#Find dcu-cli.exe
+	$EXE = Get-DellCommandUpdateLocation
+	$dclogfolder = $env:SystemDrive + '\' + 'DCUpdate'
+	New-Item -ItemType Directory $dclogfolder -force
+	$outlog = $dclogfolder + '\' + 'dellcuoutput.log'
+	$scanlog = $dclogfolder + '\' + 'scanlog.log'
+	$setlog = $dclogfolder + '\' + 'setdcu.log'
+	if ((Get-CimInstance win32_bios).Manufacturer -match 'Dell')
+	{
+		if (($EXE -eq $null) -or ((Get-Item $EXE).VersionInfo.FileVersion -ne '3.1.1.44'))
+		{
+			###Code to download andinstallstuff starts here
+			New-Item -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Force
+			New-ItemProperty -path "registry::hklm\software\policies\microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType dword -Value 2
+			
+			Write-Host "Dell command update not installed"
+			Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER06095696M/1/Dell-Command-Update-Win-32_PH01C_WIN_3.1.1_A00.EXE" -OutFile "$env:temp\DCU.exe" -UseBasicParsing -Verbose
+			start-process -FilePath "$env:temp\DCU.exe" -ArgumentList '/s' -Wait
+			Start-Sleep -Seconds 5
+		}
+		#elseif ((Get-Item $EXE).VersionInfo.FileVersionon -eq '3.1.0.64') {
+		#$dcuversion = (Get-Item $EXE).VersionInfo.FileVersion
+		$EXE = Get-DellCommandUpdateLocation
+		Write-Host "*******************Dcu cli located at $EXE*****************"
+		Write-Host "******************Set dell command update settings*********************"
+		$a = "/configure -scheduleWeekly=Mon,11:45 -updateSeverity=recommended,critical -updatetype=driver,bios -updateDeviceCategory=network,storage,audio,video,input -scheduleAction=DownloadInstallAndNotify -autoSuspendBitLocker=enable -outputLog=$setlog"
+		$set = Start-Process -FilePath $EXE -ArgumentList $a -WindowStyle Hidden -Wait -Passthru #| Out-Null
+		$set.WaitForExit()
+	<#	
+		Write-Host "******************Scanning for updates*********************"
+		$b = "/scan -outputLog=$scanlog"
+		$scan = Start-Process -FilePath $EXE -ArgumentList $b -WindowStyle Hidden -Wait -PassThru
+		$scan.WaitForExit()
+		
+		Write-Host "******************Applying updates*************************"
+		$c = "/applyUpdates -silent -outputLog=$outlog"
+		$apply = Start-Process -FilePath $EXE -ArgumentList $c -WindowStyle Hidden -Wait -PassThru
+		$apply.WaitForExit()
+		Copy-Item $dclogfolder\*.log -Destination $env:TEMP -Force -Recurse
+		Remove-Item $dclogfolder -Force -Recurse
+	#>	
+	}
+	else
+	{
+		Write-Host "This is not a dell workstation"
+	}
+	Stop-Transcript | Out-Null
 }
 
 exit $exitCode
-
